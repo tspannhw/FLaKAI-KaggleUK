10c65b3c3a55a4367c9b9046df58175ebfce6b64
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2024-11-24 17:01:19 -0500

    Update flinkworkspace.sql

--- flinkworkspace.sql
+++ flinkworkspace.sql
@@ -3,5 +3,10 @@
 ---- https://docs.confluent.io/cloud/current/flink/reference/queries/overview.html#flink-sql-queries
 ----
 SELECT avg(duration) as AverageWatchDuration, `title` as MovieTitle, count(*) as NumberofViewers 
-FROM `default`.`kaggleuk`.`audience`
+FROM `default`.`kaggleuk`.`netflixbehavior`
 GROUP BY `title`
+
+describe `default`.`kaggleuk`.`netflixbehavior`
+
+SELECT TO_TIMESTAMP(`datetime`, 'yyyy-mm-dd hh:mm:ss')
+FROM `default`.`kaggleuk`.`netflixbehavior`
