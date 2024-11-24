70af839e63599763f76efb8676f102f31caff5de
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2024-11-24 17:08:47 -0500

    Update flinkworkspace.sql

--- flinkworkspace.sql
+++ flinkworkspace.sql
@@ -10,3 +10,21 @@ describe `default`.`kaggleuk`.`netflixbehavior`
 
 SELECT TO_TIMESTAMP(`datetime`, 'yyyy-mm-dd hh:mm:ss')
 FROM `default`.`kaggleuk`.`netflixbehavior`
+
+---Analyze daily engagement patterns for each movie title. Calculate daily view counts and 
+---  total watch time for each title to track how user interest fluctuates day by day.
+
+
+SELECT `title` as MovieTitle, sum(duration) as TotalWatchTime, count(*) as DailyWatchCount,
+      DAYOFYEAR(TO_TIMESTAMP(`datetime`, 'yyyy-mm-dd hh:mm:ss')) as DayView
+FROM `default`.`kaggleuk`.`netflixbehavior`
+GROUP BY `title`, DAYOFYEAR(TO_TIMESTAMP(`datetime`, 'yyyy-mm-dd hh:mm:ss'))
+
+
+SELECT DATE_FORMAT(`datetime`, 'yyyy-mm-dd') as ViewDate
+FROM `default`.`kaggleuk`.`netflixbehavior`
+
+SELECT `title` as MovieTitle, sum(duration) as TotalWatchTime, count(*) as DailyWatchCount,
+       DATE_FORMAT(`datetime`, 'yyyy-mm-dd') as ViewDate
+FROM `default`.`kaggleuk`.`netflixbehavior`
+GROUP BY `title`, DATE_FORMAT(`datetime`, 'yyyy-mm-dd') 
