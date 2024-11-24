1796ca64912e689f2d5f4444349a079d481645ed
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2024-11-24 18:38:36 -0500

    Update flinkexercises.sql

--- flinkexercises.sql
+++ flinkexercises.sql
@@ -1,12 +1,15 @@
 
 ---- Calculate the average watch duration for each movie title across all users.
-
-SELECT avg(duration) as AverageWatchDuration, `title` as MovieTitle, count(*) as NumberofViewers 
+---- https://docs.confluent.io/cloud/current/flink/reference/queries/group-aggregation.html
+---- https://docs.confluent.io/cloud/current/flink/reference/queries/overview.html#flink-sql-queries
+----
+SELECT avg(duration) as AverageWatchDuration, `title` as MovieTitle, count(`user_id`) as NumberofViewers 
 FROM `default`.`kaggleuk`.`netflixbehavior`
 GROUP BY `title`
 
 ---- Analyze daily engagement patterns for each movie title. Calculate daily view counts and total watch time for each title to track how user interest fluctuates day by day.
-SELECT `title` as MovieTitle, DATE_FORMAT(`datetime`, 'yyyy-MM-dd') as ViewDate, count(*) as DailyViewCount, 
+
+SELECT `title` as MovieTitle, DATE_FORMAT(`datetime`, 'yyyy-MM-dd') as ViewDate, count(`user_id`) as DailyViewCount, 
   sum(duration) as TotalWatchTimeSeconds, 
   (sum(duration)/60) as TotalWatchTimeMinutes, 
   (sum(duration)/3600) as TotalWatchTimeHours
